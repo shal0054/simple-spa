f4df4e3b9db803ef49b3e3955738bdf4051f90cc
Karim Shaloh <shal0054@algonquinlive.com>
2020-12-07 08:07:42 -0500

    Removed getFace() in SEARCH

--- js/main.js
+++ js/main.js
@@ -16,60 +16,40 @@ const APP = {
       let searchBtn = document.querySelector('#btnSearch');
       searchBtn.addEventListener('click', SEARCH.search);
    },
-};
+}; // end this.nameSpace
 
 //search is for anything to do with the fetch api
 const SEARCH = {
    search(ev) {
       ev.preventDefault()
       let input = document.querySelector('input');
-      let url = `https://api.themoviedb.org/3/search/person?api_key=${APP.KEY}&query=${input.value}&language=en-US`;
-      document.getElementById('instructions').style.display = 'none';
+      let url = `${APP.baseURL}search/person?api_key=${APP.KEY}&query=${input.value}&language=en-US`;
       //log(url);
+      if (input.value) {
+         document.getElementById('instructions').style.display = 'none';
 
-      fetch(url)
-         .then(resp => {
-            if (resp.ok) {
-               return resp.json();
-            } else {
-               //did not get a HTTP 200 Status
-               throw new Error(`ERROR: ${resp.status_code} ${resp.status_message}`);
-            }
-         })
-         .then(data => {
-            ACTORS.showActors(data.results);
-         })
-         .catch(err => {
-            //handle the error
-            alert(err);
-         });
-   },
-
-   async getFace(person_id) {
-      let url = `${APP.baseURL}person/${person_id}/images?api_key=${APP.KEY}`;
-
-      let resp = await fetch(url);
-      if (resp.ok) {
-         var data = await resp.json();
-         // log(data);
+         fetch(url)
+            .then(resp => {
+               if (resp.ok) {
+                  return resp.json();
+               } else {
+                  //did not get a HTTP 200 Status
+                  throw new Error(`ERROR: ${resp.status_code} ${resp.status_message}`);
+               }
+            })
+            .then(data => {
+               ACTORS.showActors(data.results);
+               // log(data.results[0]);
+            })
+            .catch(err => {
+               //handle the error
+               alert(err);
+            });
       } else {
-         //did not get a HTTP 200 Status
-         try {
-            throw new Error(`ERROR: ${resp.status_code} ${resp.status_message}`);
-         }
-         catch (err) {
-            alert(err);
-         }
+         alert(`Please enter an actor's name.`);
       }
-      // log(data.profiles[0]);
-      if (data.profiles[0]) {
-         var imgURL = APP.IMG_BASE_URL + 'w154';
-         imgURL += await data.profiles[0].file_path;
-      } else return;
-      // log(imgURL);
-      return imgURL;
    }
-};
+}; // end SEARCH nameSpace
 
 //actors is for changes connected to content in the actors section
 const ACTORS = {
@@ -78,18 +58,18 @@ const ACTORS = {
       let df = document.createDocumentFragment();
 
       results.forEach(async (result) => {
-         let imgLink = await SEARCH.getFace(result.id);
 
-         // log(imgLink);
-         if (imgLink) {
-            // log('creating cardDiv');
+         if (result.profile_path) {
+            log('creating cardDiv');
             let cardDiv = document.createElement('div');
             cardDiv.className = "card";
+            cardDiv.setAttribute('data-actorID', result.id);
             let imageDiv = document.createElement('div');
             imageDiv.className = "image";
 
             let img = document.createElement('img');
-            img.src = imgLink;
+            img.className = "actorImage"
+            img.src = APP.IMG_BASE_URL + 'w154' + result.profile_path;
             img.alt = `${result.name}'s image`;
             imageDiv.append(img);
 
@@ -102,25 +82,44 @@ const ACTORS = {
             popularity.innerHTML = `Popularity: ${result.popularity}`;
 
             cardDiv.append(imageDiv, h3, popularity);
-            // df.append(cardDiv);
-            content.append(cardDiv);
+            df.append(cardDiv);
+            // content.append(cardDiv);
          }
       });
       content.innerHTML = '';
-      // content.append(df);
+      content.append(df);
       document.getElementById('actors').style.display = 'flex';
       document.querySelector('#actors h2').style.display = 'block';
+      content.addEventListener('click', MEDIA.showMedia)
 
    }
-};
+}; // end this.nameSpace
 
 //media is for changes connected to content in the media section
-const MEDIA = {};
+const MEDIA = {
+   showMedia(ev) {
+      if (ev.target.className === 'card' ||
+         ev.target.parentElement.className === 'card' ||
+         ev.target.className === 'actorImage') {
+         document.getElementById('actors').style.display = 'none';
+         document.getElementById('media').style.display = 'flex';
+         let mediaTitle = document.querySelector('#media h2')
+         mediaTitle.style.display = 'block';
+
+         // goes back to Actors page
+         mediaTitle.addEventListener('click', () => {
+            document.getElementById('media').style.display = 'none';
+            document.getElementById('actors').style.display = 'flex';
+            document.querySelector('#actors h2').style.display = 'block';
+         });
+      } // end if
+   } // end func
+}; // end this.nameSpace
 
 //storage is for working with localstorage
 const STORAGE = {
    //this will be used in Assign 4
-};
+}; // end this.nameSpace
 
 //nav is for anything connected to the history api and location
 const NAV = {
